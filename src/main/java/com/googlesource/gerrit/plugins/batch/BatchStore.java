9c775cf0d0106ca7170462260ecb20d0abb17846
Nicholas Legg <nlegg@codeaurora.org>
2018-03-06 14:55:24 -0700

    Create a batch service plugin
    The batch plugin provides a mechanism for building and previewing sets
    of proposed updates to multiple project/branches/refs that should be
    applied in a batch. These updates are built with Gerrit changes.
    
    This initial change provides a mechanism to merge a change to a batch
    with a single command.
    
    Change-Id: I860fa82dfa6cfe2029c534c55593e580c6f8afc6

---
+++ src/main/java/com/googlesource/gerrit/plugins/batch/BatchStore.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.batch;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ConcurrentModificationException;
+import javax.inject.Singleton;
+
+@Singleton
+public class BatchStore {
+  protected Path base;
+  protected Gson gson = new Gson();
+
+  @Inject
+  public BatchStore(SitePaths site) throws IOException {
+    base = site.site_path.resolve("data").resolve("plugin").resolve("batch");
+    createDirectories(base);
+  }
+
+  public void save(Batch batch) throws IOException {
+    String json = gson.toJson(batch);
+    if (batch.version != 0) {
+      throw new ConcurrentModificationException();
+    }
+    batch.version++;
+    Files.write(base.resolve(batch.id), json.getBytes(UTF_8));
+  }
+
+  /* A drop in replacement for Files.createDirectories that works even when the
+   * tail of Path is a link
+   */
+  protected static Path createDirectories(Path path) throws IOException {
+    try {
+      return Files.createDirectories(path);
+    } catch (FileAlreadyExistsException e) {
+      return Files.createDirectories(Files.readSymbolicLink(path));
+    }
+  }
+}
