a0c53a94b6dc06dad3f3a6e62bd2003fccbec698
Nicholas Legg <nlegg@codeaurora.org>
2018-03-06 14:55:24 -0700

    Create a batch service plugin
    The batch plugin provides a mechanism for building and previewing sets
    of proposed updates to multiple project/branches/refs that should be
    applied in a batch. These updates are built with Gerrit changes.
    
    This initial change provides a mechanism to merge a change to a batch
    with a single command.
    
    Change-Id: I860fa82dfa6cfe2029c534c55593e580c6f8afc6

---
+++ src/main/java/com/google/gerrit/server/git/meta/GitFile.java
@@ -0,0 +1,95 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git.meta;
+
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.File;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import java.io.IOException;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.CommitBuilder;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+/** A GitFile is a text file (UTF8) from a git repository */
+public class GitFile extends VersionedMetaData {
+  public interface Factory {
+    GitFile create(@Assisted File.NameKey file);
+  }
+
+  protected final MetaDataUpdate.User metaDataUpdateFactory;
+  protected final GitRepositoryManager repos;
+
+  protected Branch.NameKey branch;
+  protected String file;
+
+  public String text;
+
+  @Inject
+  public GitFile(
+      MetaDataUpdate.User metaDataUpdateFactory,
+      GitRepositoryManager repos,
+      @Assisted File.NameKey file) {
+    this.metaDataUpdateFactory = metaDataUpdateFactory;
+    this.repos = repos;
+    this.branch = file.getParentKey();
+    this.file = file.get();
+  }
+
+  public String read() throws ConfigInvalidException, IOException, NoSuchProjectException {
+    try (Repository repo = repos.openRepository(branch.getParentKey())) {
+      load(repo);
+      return text;
+    }
+  }
+
+  public RevCommit write(String fileContent, String commitMessage)
+      throws ConfigInvalidException, IOException, NoSuchProjectException {
+    try (MetaDataUpdate md = metaDataUpdateFactory.create(branch.getParentKey())) {
+      load(md);
+      text = fileContent;
+      md.getCommitBuilder().setCommitter(metaDataUpdateFactory.getUserPersonIdent());
+      md.setMessage(commitMessage);
+      return commit(md);
+    }
+  }
+
+  public void setBranch(Branch.NameKey branch) {
+    this.branch = branch;
+  }
+
+  public void setFileName(String fileName) {
+    this.file = fileName;
+  }
+
+  @Override
+  protected String getRefName() {
+    return branch.get();
+  }
+
+  @Override
+  protected void onLoad() throws IOException {
+    text = readUTF8(file);
+  }
+
+  @Override
+  protected boolean onSave(CommitBuilder commit) throws IOException {
+    saveUTF8(file, text);
+    return true;
+  }
+}
---
+++ src/main/java/com/googlesource/gerrit/plugins/batch/Batch.java
@@ -0,0 +1,104 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.batch;
+
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/** Data class (for serialization) to represent the contents/state of a batch */
+public class Batch {
+  public enum State {
+    OPEN,
+    CLOSED;
+  }
+
+  public static class Change {
+    int number;
+    int patchSet;
+
+    Change(PatchSet.Id psId) {
+      number = psId.getParentKey().get();
+      patchSet = psId.get();
+    }
+  }
+
+  public class Destination {
+    public String project;
+    public String ref;
+    public String sha1;
+    public String downloadRef;
+    public List<Change> changes;
+
+    public void add(PatchSet.Id psId) {
+      if (changes == null) {
+        changes = new ArrayList<Change>();
+      }
+      changes.add(new Batch.Change(psId));
+    }
+  }
+
+  public final String id;
+  public Integer version;
+  public Account.Id owner;
+  public State state;
+  public List<Destination> destinations;
+  public Date lastModified;
+
+  public Batch(Account.Id owner) {
+    this.id = UUID.randomUUID().toString();
+    this.version = 0;
+    this.state = State.OPEN;
+    this.owner = owner;
+  }
+
+  /** Get a non-null list without modifying the batch. */
+  public List<Destination> listDestinations() {
+    if (destinations == null) {
+      return Collections.emptyList();
+    }
+    return destinations;
+  }
+
+  public Destination getDestination(Branch.NameKey branch) {
+    if (destinations == null) {
+      destinations = new ArrayList<Destination>();
+    }
+    Destination dest = getExistingDestination(branch);
+    if (dest == null) {
+      dest = new Destination();
+      dest.project = branch.getParentKey().get();
+      dest.ref = branch.get();
+      destinations.add(dest);
+    }
+    return dest;
+  }
+
+  protected Destination getExistingDestination(Branch.NameKey branch) {
+    if (destinations == null) {
+      destinations = new ArrayList<Destination>();
+    }
+    for (Destination dest : destinations) {
+      if (dest.project.equals(branch.getParentKey().get()) && dest.ref.equals(branch.get())) {
+        return dest;
+      }
+    }
+    return null;
+  }
+}
