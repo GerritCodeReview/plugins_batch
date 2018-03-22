286400da6df2947a1a93a6597c657b8cd44be2f7
Nicholas Legg <nlegg@codeaurora.org>
2018-03-22 13:49:04 -0600

    Add delete command - delete an existing batch
    Allows the user to delete a batch created with the batch plugin.
    
    Change-Id: I331b2675597a8b342b950b37edc36ac29843e8f0

--- src/main/java/com/googlesource/gerrit/plugins/batch/BatchStore.java
+++ src/main/java/com/googlesource/gerrit/plugins/batch/BatchStore.java
@@ -48,6 +48,10 @@ public class BatchStore {
   }
 
   public void save(Batch batch) throws IOException, NoSuchProjectException {
+    if (batch.state == Batch.State.DELETED) {
+      refUpdater.delete(getBranch(batch.id));
+      return;
+    }
     if (batch.version != 0) {
       throw new ConcurrentModificationException();
     }
